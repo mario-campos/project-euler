f6e703ff8de618e62cdeb5247d2743434ed0da8f
mac <mac@laptop>
2016-01-27 13:38:07 -0600

    replaced  with
    because  sent the program into an infinite loop.  is what I wanted all along.

--- src/1.hs
+++ src/1.hs
@@ -4,7 +4,7 @@ multiples :: Int -> [Int]
 multiples x = iterate (+x) x
 
 euler1 n = sum $ toList $ union mults3 mults5
-  where mults3 = fromDistinctAscList $ filter (<n) (multiples 3)
-        mults5 = fromDistinctAscList $ filter (<n) (multiples 5)
+  where mults3 = fromDistinctAscList $ takeWhile (<n) (multiples 3)
+        mults5 = fromDistinctAscList $ takeWhile (<n) (multiples 5)
 
 main = print (euler1 1000)
